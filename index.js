b4a219ccab4f166c565da374026034e1a23bee54
Julie Evans <juliemevans17@gmail.com>
2021-10-16 16:10:39 -0500

    commit tree 55b4dc10cd6ad114f1c369c0736f5ab709206216

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Your code here
+function saturdayFun(someValue = "roller-skate") {
+    return `This Saturday, I want to ${someValue}!`
+}
+function mondayWork(someValue = "go to the office") {
+    return `This Monday, I will ${someValue}.`
+}
+function wrapAdjective(highlight = "*") {
+    return function (prompt = "special") {
+        return `You are ${highlight}${prompt}${highlight}!`
+    }
+}
